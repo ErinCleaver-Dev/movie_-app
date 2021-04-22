2a1fc58bffbc29a173de84883b202304028506aa
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-04-22 06:39:22 -0400

    Added the ability to add pictures

--- movies/views.py
+++ movies/views.py
@@ -10,4 +10,23 @@ def home_page(request):
     return render(request, 'movies/movies_stuff.html', stuff_for_frontend)
 
 def create(request):
+    if request.method == 'POST':
+        data = {
+            'Name': request.POST.get('name'),
+            'Picture': request.POST.get('picture'),
+            'Rating': int(request.POST.get('rating')),
+            'Notes': request.POST.get('notes'),
+        }
+        print(data)
+        try:
+            response = Movie.objects.create(
+                name=data.get('Name'),
+                picture=data.get('Picture'),
+                rating=data.get('Rating'),
+                notes=data.get('Notes')
+            )
+        except Exception as e:
+            print(e)
+            pass 
+
     return redirect('/')
